5f5db99f724dcec533edb9a0a5be84b1ada2053f
ivan-iglesias <iiglesiasbilbao@gmail.com>
2019-06-28 10:02:43 +0200

    Correccion en el calculo de horas nocturnas

--- app/Model/Record/NightShift.php
+++ app/Model/Record/NightShift.php
@@ -10,37 +10,50 @@ class NightShift
 {
 	/**
 	 * Calculo el tiempo trabajado en horario nocturno en segundos.
+	 *
 	 */
 	public static function getTimeInSeconds(Carbon $checkIn, Carbon $checkOut)
 	{
-		$start = Helpers::timeToSeconds(config('options.night_shift.start'));	// 22:00
-		$end = Helpers::timeToSeconds(config('options.night_shift.end'));		// 06:00
+		$diffDay = $checkIn->day !== $checkOut->day;
 		$checkIn = Helpers::timeToSeconds($checkIn->format('H:i:s'));
 		$checkOut = Helpers::timeToSeconds($checkOut->format('H:i:s'));
 
-		if ($checkIn >= $end && $checkIn <= $start) {
-			// 09:00 - 18:00 (Horario normal)
-			if ($checkOut >= $end && $checkOut <= $start) { return 0; }
+		// Comienzo y final del horario nocturno (incluidas)
+		$start = Helpers::timeToSeconds(config('options.night_shift.start'));	// 22:00
+		$end = Helpers::timeToSeconds(config('options.night_shift.end'));		// 06:00
 
-			// 09:00 - 23:15 (Termino en horario nocturno)
-			if ($checkOut > $start) { return $checkOut - $start; }
-
-			// 09:00 - 02:15 (Termino en horario nocturno de madrugada)
-			return (86400 - $start) + $checkOut;
-		}
-
-		if ($checkIn > $start) {
+		// Empiezo en horario nocturno (noche)
+		if ($checkIn >= $start) {
 			// 23:15 - 23:45
-			if ($checkOut > $start) { return $checkOut - $checkIn; }
+			if ($checkOut >= $start) { return $checkOut - $checkIn; }
 
 			// 23:15 - 02:00
-			return (86400 - $checkIn) + $checkOut;
+			if ($checkOut <= $end) { return (86400 - $checkIn) + $checkOut; }
+
+			// 23:15 - 06:05
+			return (86400 - $checkIn) + $end;
 		}
 
-		// 05:00 - 11:00 (Empiezo en horario nocturno)
-		if ($checkIn <= $end && ($checkOut > $end && $checkOut < $start)) { return $end - $checkIn; }
+		// Empiezo en horario diurno
+		if ($checkIn > $end) {
+			// 09:00 - 23:15 (Termino en horario nocturno)
+			if ($checkOut >= $start) { return $checkOut - $start; }
 
+			// 09:00 - 02:15 (Termino en horario nocturno de madrugada)
+			if ($checkOut <= $end) { return (86400 - $start) + $checkOut; }
+
+			// 09:00 - 18:00 Empieza y termina en horario diurno (puede ser del mismo dia o diferente)
+			return $diffDay ? (86400 - $start) + $end : 0;
+		}
+
+		// Empiezo en horario nocturno (madrugada)
 		// 01:15 - 02:00
-		return $checkOut- $checkIn;
+		if ($checkOut <= $end) { return $checkOut - $checkIn; }
+
+		// 05:00 - 23:00
+		if ($checkOut >= $start) { return ($end - $checkIn) + ($checkOut - $start); }
+
+		// 05:00 - 11:00
+		return $end - $checkIn;
 	}
 }
